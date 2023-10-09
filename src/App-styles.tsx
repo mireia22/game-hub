eb58c009f47ecd72d555377ba50de3e036e54074
mireia22 <mireiagarciaferrer22@gmail.com>
2023-10-09 19:53:11 +0200

    update dashboard background

--- src/App-styles.tsx
+++ src/App-styles.tsx
@@ -3,19 +3,19 @@ import { styled } from "styled-components";
 export const AppWrp = styled.div<{ isdashboard: string }>`
   background-image: url(${({ isdashboard }) =>
     isdashboard === "true"
-      ? "https://www.transparenttextures.com/patterns/purty-wood.png"
+      ? "https://www.transparenttextures.com/patterns/asfalt-light.png"
       : "/assets/backgrounds/mobile-background-2.jpg"});
 
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center; // Center the background image
   background-color: ${({ isdashboard }) =>
-    isdashboard === "true" ? "#d47208e0" : "transparent"};
+    isdashboard === "true" ? "#ef8b20e4" : "transparent"};
 
   @media (min-width: 630px) {
     background-image: url(${({ isdashboard }) =>
       isdashboard === "true"
-        ? "https://www.transparenttextures.com/patterns/purty-wood.png"
+        ? "https://www.transparenttextures.com/patterns/asfalt-light.png"
         : "/assets/backgrounds/desktop-background.jpg"});
   }
 `;
