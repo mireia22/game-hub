9a7931d48a906a62dac482c93d0bb6e5fe00899a
mireia22 <mireiagarciaferrer22@gmail.com>
2023-10-10 06:55:58 +0200

    change dashboard background

--- src/App-styles.tsx
+++ src/App-styles.tsx
@@ -10,7 +10,7 @@ export const AppWrp = styled.div<{ isdashboard: string }>`
   background-repeat: no-repeat;
   background-position: center; // Center the background image
   background-color: ${({ isdashboard }) =>
-    isdashboard === "true" ? "#fbf0e1" : "transparent"};
+    isdashboard === "true" ? "#fbf8e7" : "transparent"};
 
   @media (min-width: 630px) {
     background-image: url(${({ isdashboard }) =>
